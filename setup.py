8cc2112d3fce6f66a56116b0f6ebaf64f6cb68b4
Joon-Yong Lee <joonyong.lee@pnnl.gov>
2017-06-09 16:43:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,22 @@ if sys.platform == 'darwin':
     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
     os.environ["CC"] = "/usr/local/bin/clang-omp++"
     os.environ['CXX'] = "/usr/local/bin/clang-omp++"
+    bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'BSFCoreDll.cpp'],
+                        language="c++",
+                        # extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g', '-O0'],
+                        extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g'],
+                        extra_link_args=["-fopenmp"],
+                        include_dirs=[numpy.get_include()]
+                        )
 else:
-    os.environ["CC"] = "g++"
-    os.environ['CXX'] = "g++"
-bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'bsf-core/BSFCoreDll.cpp'],
-                    language="c++",
-                    extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g'],
-                    extra_link_args=["-fopenmp"],
-                    include_dirs=[numpy.get_include()]
-                    )
+    os.environ["CC"] = "/usr/bin/g++"
+    os.environ['CXX'] = "/usr/bin/g++"
+    bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'BSFCoreDll.cpp'],
+                        language="c++",
+                        extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g'],
+                        extra_link_args=["-fopenmp"],
+                        include_dirs=[numpy.get_include()]
+                        )
 
 setup(name='bsf',
       version='1.0',
