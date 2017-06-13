23e82e7587eb025775d5206bd05b19e075499396
Joon-Yong Lee <joonyong.lee@pnnl.gov>
2017-06-13 10:57:49 -0700

    fix the file path

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.platform == 'darwin':
     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
     os.environ["CC"] = "/usr/local/bin/clang-omp++"
     os.environ['CXX'] = "/usr/local/bin/clang-omp++"
-    bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'BSFCoreDll.cpp'],
+    bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'bsf-core/BSFCoreDll.cpp'],
                         language="c++",
                         # extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g', '-O0'],
                         extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g'],
@@ -19,7 +19,7 @@ if sys.platform == 'darwin':
 else:
     os.environ["CC"] = "/usr/bin/g++"
     os.environ['CXX'] = "/usr/bin/g++"
-    bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'BSFCoreDll.cpp'],
+    bsf_mod = Extension('bsf', sources=['bsf_py.cpp', 'bsf-core/BSFCoreDll.cpp'],
                         language="c++",
                         extra_compile_args=['-fopenmp', '-std=c++11', '-mpopcnt', '-g'],
                         extra_link_args=["-fopenmp"],
